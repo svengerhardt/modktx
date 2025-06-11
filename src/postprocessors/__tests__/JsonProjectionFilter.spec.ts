1bd01b31a729336ef6a38f986dfa1ee4ee20e34d
Sven Gerhardt <sven.gerhardt@trilobyte-se.de>
2025-06-11 22:33:54 +0200

    clean up

--- src/postprocessors/__tests__/JsonProjectionFilter.spec.ts
+++ src/postprocessors/__tests__/JsonProjectionFilter.spec.ts
@@ -6,28 +6,32 @@ describe('JsonProjectionFilter', () => {
       root: {
         candles: [
           { time: 't1', open: 1, close: 2 },
-          { time: 't2', open: 3, close: 4 }
+          { time: 't2', open: 3, close: 4 },
         ],
         indicators: [
           { time: 't1', rsi: 30, ema: 1 },
-          { time: 't2', rsi: 35, ema: 2 }
-        ]
-      }
+          { time: 't2', rsi: 35, ema: 2 },
+        ],
+      },
     }
 
-    const filter = new JsonProjectionFilter({ time: true, close: true, rsi: true })
+    const filter = new JsonProjectionFilter({
+      time: true,
+      close: true,
+      rsi: true,
+    })
     const result = JSON.parse(await filter.postProcess(JSON.stringify(input)))
     expect(result).toEqual({
       root: {
         candles: [
           { time: 't1', close: 2 },
-          { time: 't2', close: 4 }
+          { time: 't2', close: 4 },
         ],
         indicators: [
           { time: 't1', rsi: 30 },
-          { time: 't2', rsi: 35 }
-        ]
-      }
+          { time: 't2', rsi: 35 },
+        ],
+      },
     })
   })
 
@@ -42,25 +46,24 @@ describe('JsonProjectionFilter', () => {
       root: {
         candles: [
           { time: 't1', open: 1, close: 2 },
-          { time: 't2', open: 3, close: 4 }
+          { time: 't2', open: 3, close: 4 },
         ],
         indicators: [
           { time: 't1', rsi: 30 },
-          { time: 't2', rsi: 35 }
-        ]
-      }
+          { time: 't2', rsi: 35 },
+        ],
+      },
     }
-    const filter = new JsonProjectionFilter({ time: true, close: true, rsi: true }, 1)
+    const filter = new JsonProjectionFilter(
+      { time: true, close: true, rsi: true },
+      1,
+    )
     const result = JSON.parse(await filter.postProcess(JSON.stringify(input)))
     expect(result).toEqual({
       root: {
-        candles: [
-          { time: 't2', close: 4 }
-        ],
-        indicators: [
-          { time: 't2', rsi: 35 }
-        ]
-      }
+        candles: [{ time: 't2', close: 4 }],
+        indicators: [{ time: 't2', rsi: 35 }],
+      },
     })
   })
 })
